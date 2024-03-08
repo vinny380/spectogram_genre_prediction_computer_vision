0e4fff7329714cdb171886fd1d075822fa637715
Vangekillsvado <aclarke500@gmail.com>
2024-03-08 18:04:30 -0500

    updated parameters

--- model.py
+++ model.py
@@ -6,6 +6,7 @@ from sklearn.preprocessing import StandardScaler
 from torch.utils.data import TensorDataset, DataLoader
 from Data import load_data
 
+
 X, y = load_data()
 print(X.shape, y.shape)
 # Split the dataset into training and testing sets
@@ -41,7 +42,7 @@ test_loader = DataLoader(dataset=test_dataset, batch_size=16, shuffle=False)
 class VerusNet(nn.Module):
     def __init__(self):
         super(VerusNet, self).__init__()
-        self.fc1 = nn.Linear(2400, 100)  # 4 input features, 100 neurons in first hidden layer
+        self.fc1 = nn.Linear(2400, 100)  # 2400 input features, 100 neurons in first hidden layer
         self.fc2 = nn.Linear(100, 50) # Second hidden layer
         self.fc3 = nn.Linear(50, 2)   # 3 output classes
 
@@ -57,10 +58,10 @@ model = VerusNet()
 
 # Define the loss function and optimizer
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+optimizer = optim.Adam(model.parameters(), lr=0.45)
 
 # Train the model
-epochs = 100
+epochs = 37
 for epoch in range(epochs):
     for inputs, labels in train_loader:
         optimizer.zero_grad()
@@ -81,4 +82,5 @@ with torch.no_grad():
         total += labels.size(0)
         correct += (predicted == labels).sum().item()
 
-print(f'Accuracy on test set: {100 * correct / total}%')
+# print(f'Accuracy on test set: {100 * correct / total}%')
+print({100 * correct / total})
